d3e6697d651d005a9e82879cee60ddfb3ddd497a
Ajibade <ajibadeemmanuel58@gmail.com>
2023-12-28 22:52:34 +0100

    sponsor entity

---
+++ models/Task.js
@@ -0,0 +1,28 @@
+// models/Task.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../src/connection/db');
+
+const Task = sequelize.define('tasks', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+
+module.exports = Task;
