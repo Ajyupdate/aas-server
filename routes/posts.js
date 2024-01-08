fa608563a71690543b771f605567390618e7c3d1
Ajibade <ajibadeemmanuel58@gmail.com>
2024-01-08 19:53:34 +0100

    testing paystack

--- routes/posts.js
+++ routes/posts.js
@@ -24,7 +24,7 @@ router.get('', async (req, res) => {
 
 router.get("/:id", async(req, res) => {
   const id = req.params.id
-  console.log(id, "what is it")
+ 
   try{
     const post = await Post.findByPk(id, {
       include: [{
